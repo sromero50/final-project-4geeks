d63e4aabcbf2c686fda2b7c1380a20063033da36
sromero50 <seba98asd@hotmail.com>
2021-11-06 00:25:51 +0000

    update models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, Empresa, Administrador, Linea, Parada, Horario, Reserva, Usuario
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,7 +11,14 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Empresa, db.session))
+    admin.add_view(ModelView(Administrador, db.session))
+    admin.add_view(ModelView(Usuario, db.session))
+    admin.add_view(ModelView(Linea, db.session))
+    admin.add_view(ModelView(Parada, db.session))
+    admin.add_view(ModelView(Horario, db.session))
+    admin.add_view(ModelView(Reserva, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -3,13 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Empresa(db.Model):
-    __tablename__ = 'empresa'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-        
-    
+    linea = db.relationship("Linea", backref="empresa")
 
     def __repr__(self):
         return '<Empresa %r>' % self.nombre
@@ -24,7 +22,6 @@ class Empresa(db.Model):
         }
 
 class Administrador(db.Model):
-    __tablename__ = 'administrador'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -42,10 +39,34 @@ class Administrador(db.Model):
            
         }
 
-class Linea(db.Model):
-    __tablename__ = 'linea'
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    reserva = db.relationship("Reserva", backref="usuario")
+
+    def __repr__(self):
+        return '<Usuario %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "email": self.email,
+            "password": self.password
+           
+        }
+
+class Linea(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_empresa = db.Column(db.Integer, db.ForeignKey('empresa.id'))
     numero_linea = db.Column(db.String(80), unique=False, nullable=False)
+    origen = db.Column(db.String(80), unique=False, nullable=False)
+    destino = db.Column(db.String(80), unique=False, nullable=False)
+    horario = db.relationship("Horario", backref="linea")
+    reserva = db.relationship("Reserva", backref="linea")
 
     def __repr__(self):
         return '<Linea %r>' % self.numero_linea
@@ -53,14 +74,17 @@ class Linea(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "numero_linea": self.numero_linea
+            "numero_linea": self.numero_linea,
+            "origen": self.origen,
+            "destino": self.destino
            
         }
 
 class Parada(db.Model):
-    __tablename__ = 'parada'
+
     id = db.Column(db.Integer, primary_key=True)
-    ubicacion = email = db.Column(db.String(120), unique=True, nullable=False)
+    ubicacion = db.Column(db.String(120), unique=True, nullable=False)
+    horario = db.relationship("Horario", backref="parada")
 
     def __repr__(self):
         return '<Parada %r>' % self.ubicacion
@@ -73,12 +97,13 @@ class Parada(db.Model):
         }
 
 class Horario(db.Model):
-    __tablename__ = 'horario'
+
     id = db.Column(db.Integer, primary_key=True)
     id_linea = db.Column(db.Integer, db.ForeignKey('linea.id'))
     id_parada = db.Column(db.Integer, db.ForeignKey('parada.id'))
     tipo_dia = db.Column(db.String(80), unique=False, nullable=False)
     hora = db.Column(db.String(80), unique=False, nullable=False)
+    reserva = db.relationship("Reserva", backref="horario")
 
     def __repr__(self):
         return '<Horario %r>' % self.hora
@@ -93,8 +118,22 @@ class Horario(db.Model):
            
         }
 
-# class Parada_nombre(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     parada_ubicacion = db.Column(db.String(120), db.ForeignKey('parada.ubicacion'))
-#     tipo_dia = db.Column(db.String(120), db.ForeignKey('horario.tipo_dia'))
-#     hora = db.Column(db.String(120), db.ForeignKey('horario.hora'))
+class Reserva(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_linea = db.Column(db.Integer, db.ForeignKey('linea.id'))
+    id_horario = db.Column(db.Integer, db.ForeignKey('horario.id'))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    asiento = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Reserva %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_linea": self.id_linea,
+            "id_horario": self.id_horario,
+            "asiento": self.asiento
+           
+        }
+
