f69a3fd43bd85245aa96003e9503f7f794b5e143
mariaperrone <perronemar148@gmail.com>
2021-11-20 01:08:48 +0000

    vista admin y notFound

--- src/front/js/component/addEmpresa.js
+++ src/front/js/component/addEmpresa.js
@@ -19,7 +19,7 @@ const AddEmpresa = props => {
 			<div className="empresaInput container">
 				<div>
 					<form onSubmit={handleSubmit}>
-						<div className="inputEmpresa">
+						<div className="empresaInput row">
 							<input
 								type="text"
 								className="form-control my-2"
